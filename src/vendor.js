0438ff2e9bf7b847dca0621beac434f34ebd613d
CiaoFella <83582051+CiaoFella@users.noreply.github.com>
2025-01-14 17:37:59 +0100

    Initial commit

---
+++ src/vendor.js
@@ -0,0 +1,9 @@
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger.js'
+import barba from '@barba/core'
+import SplitType from 'split-type'
+import Lenis from '@studio-freight/lenis'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export { gsap, ScrollTrigger, barba, SplitType, Lenis }
