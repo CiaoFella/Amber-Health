62c5109edc7cb2cca30ef065605b08c42dff8eab
CiaoFella <ciao@julianfella.de>
2025-01-24 19:52:39 +0100

    Enhance animation modules and styles for improved user experience
    - Added new animation modules: navBar.js, bgGradient.js, and serviceOverview.js, integrating GSAP for smooth transitions.
    - Updated hero.js to refine animation logic and improve element selection with new data attributes.
    - Introduced hero.scss for specific styles related to hero animations, enhancing visual effects.
    - Modified cursor.scss to improve cursor animations and visual feedback.
    - Removed unused enterPage.scss file to streamline styles.
    - Updated style.scss to include the new hero styles for comprehensive animation support.

--- src/animations/shared.js
+++ src/animations/shared.js
@@ -1,14 +1,32 @@
 import footer from './general/footer.js'
+import navBar from './general/navBar.js'
+import bgGradient from './shared/bgGradient.js'
+import buttons from './shared/buttons.js'
+import elementReveal from './shared/elementReveal.js'
 import hero from './shared/hero.js'
+import serviceOverview from './shared/serviceOverview.js'
+import textReveal from './shared/textReveal.js'
 
 function init() {
   hero.init()
   footer.init()
+  bgGradient.init()
+  textReveal.init()
+  elementReveal.init()
+  footer.init()
+  buttons.init()
+  navBar.init()
+  serviceOverview.init()
 }
 
 function cleanup() {
   hero.cleanup()
   footer.cleanup()
+  bgGradient.cleanup()
+  textReveal.cleanup()
+  elementReveal.cleanup()
+  navBar.cleanup()
+  serviceOverview.cleanup()
 }
 
 export default {
